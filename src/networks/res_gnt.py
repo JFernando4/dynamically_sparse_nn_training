c4da02d15cd166543f45d64049d4c34c29ad4067
Jfernando4 <jfhernan@ualberta.ca>
2023-11-22 10:07:30 -0700

    update: added missing function

--- src/networks/res_gnt.py
+++ src/networks/res_gnt.py
@@ -1,12 +1,24 @@
 from torch.nn import Conv2d, Linear, BatchNorm2d
 from torch import where, rand, topk, long, empty, zeros, no_grad, tensor
+from math import sqrt
 import torch
 import sys
 from torch.nn.init import calculate_gain
 
 
-def get_layer_bound():
-    return
+def get_layer_bound(layer, init, gain):
+    if isinstance(layer, Conv2d):
+        return sqrt(1 / (layer.in_channels * layer.kernel_size[0] * layer.kernel_size[1]))
+    elif isinstance(layer, Linear):
+        if init == 'default':
+            bound = sqrt(1 / layer.in_features)
+        elif init == 'xavier':
+            bound = gain * sqrt(6 / (layer.in_features + layer.out_features))
+        elif init == 'lecun':
+            bound = sqrt(3 / layer.in_features)
+        else:
+            bound = gain * sqrt(3 / layer.in_features)
+        return bound
 
 
 class ResGnT(object):
