61fca7e59d152c1627034490799f40563146c865
Jfernando4 <jfhernan@ualberta.ca>
2023-11-24 10:57:23 -0700

    update: added function for storing results in memory

--- experiments/resnet_incremental_cifar_demonstration/post_run_analysis.py
+++ experiments/resnet_incremental_cifar_demonstration/post_run_analysis.py
@@ -53,22 +53,23 @@ def load_classes(classes_dir_path: str, index: int):
     return np.load(classes_file_path)
 
 
-def load_cifar_data(data_path: str) -> (CifarDataSet, DataLoader):
+def load_cifar_data(data_path: str, train: bool = True) -> (CifarDataSet, DataLoader):
     """
     Loads the cifar 100 data set with normalization
     :param data_path: path to the directory containing the data set
+    :param train: bool that indicates whether to load the train or test data
     :return: torch DataLoader object
     """
     cifar_data = CifarDataSet(root_dir=data_path,
-                              train=True,
+                              train=train,
                               cifar_type=100,
                               device=None,
                               image_normalization="max",
                               label_preprocessing="one-hot",
                               use_torch=True)
 
-    mean = (0.4914, 0.4822, 0.4465)
-    std = (0.2470, 0.2435, 0.2616)
+    mean = (0.5071, 0.4865, 0.4409)
+    std = (0.2673, 0.2564, 0.2762)
 
     transformations = [
         ToTensor(swap_color_axis=True),  # reshape to (C x H x W)
@@ -127,10 +128,9 @@ def compute_dormant_units_proportion(net: ResNet, cifar_data_loader: DataLoader,
     return dead_neurons.sum().item() / number_of_features, last_layer_activations.numpy()
 
 
-def compute_effective_rank(last_layer_features: np.ndarray):
+def compute_effective_rank(singular_values: np.ndarray):
     """ Computes the effective rank of the representation layer """
 
-    singular_values = svd(last_layer_features, compute_uv=False, lapack_driver="gesvd")
     norm_sv = singular_values / np.sum(np.abs(singular_values))
     entropy = 0.0
     for p in norm_sv:
@@ -140,18 +140,90 @@ def compute_effective_rank(last_layer_features: np.ndarray):
     return np.e ** entropy
 
 
-def compute_stable_rank(last_layer_features: np.ndarray):
-
-    singular_values = svd(last_layer_features, compute_uv=False, lapack_driver="gesvd")
+def compute_stable_rank(singular_values: np.ndarray):
+    """ Computes the stable rank of the representation layer """
     sorted_singular_values = np.flip(np.sort(singular_values))
     cumsum_sorted_singular_values = np.cumsum(sorted_singular_values) / np.sum(singular_values)
     return np.sum(cumsum_sorted_singular_values < 0.99) + 1
+
+
+@torch.no_grad()
+def compute_last_task_accuracy_per_class_in_order(net: torch.nn.Module, ordered_classes: np.ndarray,
+                                                  test_data: DataLoader, experiment_index: int):
+    """
+    Computes the accuracy of each class in the order they were presented
+    :param net: resnet with the parameters stored at the end of the experiment
+    :param ordered_classes: numpy array with the cifar 100 classes in the order they were presented
+    :param test_data: cifar100 test data
+    :param experiment_index: index of the experiment
+    :return: numpy array
+    """
+
+    device = net.fc.weight.device
+    num_classes = 100
+    num_examples_per_class = 100
+
+    class_correct = torch.zeros(num_classes, dtype=torch.float32, device=device)
+    for i, sample in enumerate(test_data):
+        image = sample["image"].to(device)
+        labels = sample["label"].to(device)
+        outputs = net(image)
+        _, predicted = torch.max(outputs, 1)    # Get the class with the highest score
+        _, labels = torch.max(labels, 1)        # Get the class with the highest score
+
+        # Update the counts for each class
+        for i in range(num_classes):
+            class_correct[i] += (predicted == labels).masked_select(labels == i).sum().item()
+
+    return class_correct.cpu().numpy() / num_examples_per_class
+
+
+# -------------------- For storing the results of the analysis -------------------- #
+def store_analysis_results(weight_magnitude_results: np.ndarray,
+                           dormant_units_results: (np.ndarray, np.ndarray),
+                           effective_rank_results: (np.ndarray, np.ndarray),
+                           stable_rank_results: (np.ndarray, np.ndarray),
+                           accuracy_per_class_in_order: np.ndarray,
+                           results_dir: str, experiment_index: int):
+    """
+    Stores the results of the post run analysis
+    :param weight_magnitude_results: np array containing the output of compute_average_weight_magnitude
+    :param dormant_units_results: tuple containing the results of the dormant unit analysis for the previous tasks and
+                                  the next task for each different task
+    :param effective_rank_results: tuple containing the results of the effective rank analysis for the previous tasks
+                                   and the next task for each different task
+    :param stable_rank_results: tuple containing the results of the stable rank analysis for the previous tasks and the
+                                next task for each different task
+    :param accuracy_per_class_in_order: np array containing the accuracy of the final model for each class in the order
+                                        they were presented
+    :param results_dir: path to the results directory
+    :param experiment_index: experiment index
+    """
+
+    index_file_name = "index-{0}.npy".format(experiment_index)
+    result_dir_names_and_arrays = [
+        ("weight_magnitude_analysis", weight_magnitude_results),
+        ("previous_tasks_dormant_units_analysis", dormant_units_results[0]),
+        ("next_task_dormant_units_analysis", dormant_units_results[1]),
+        ("previous_tasks_effective_rank_analysis", effective_rank_results[0]),
+        ("next_task_effective_rank_analysis", effective_rank_results[1]),
+        ("previous_tasks_stable_rank_analysis", stable_rank_results[0]),
+        ("next_task_stable_rank_analysis", stable_rank_results[1]),
+        ("accuracy_per_class_in_order", accuracy_per_class_in_order)
+    ]
+
+    # store results in the corresponding dir
+    for results_name, results_array in result_dir_names_and_arrays:
+        temp_results_dir = os.path.join(results_dir, results_name)
+        os.makedirs(temp_results_dir, exist_ok=True)
+        np.save(os.path.join(temp_results_dir, index_file_name), results_array)
 
 
 def analyze_results(results_dir: str, data_path: str, dormant_unit_threshold: float = 0.01):
     """
     Analyses the parameters of a run and creates files with the results of the analysis
     :param results_dir: path to directory containing the results for a parameter combination
+    :param data_path: path to the cifar100 data set
     :param dormant_unit_threshold: hidden units whose activation fall bellow this threshold are considered dormant
     """
 
@@ -162,14 +234,16 @@ def analyze_results(results_dir: str, data_path: str, dormant_unit_threshold: fl
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     number_of_epochs = np.arange(21) * 200  # by design the model parameters where store after each of these epochs)
     classes_per_task = 5                    # by design each task increases the data set by 5 classes
+    last_epoch = 4000
     experiment_indices = np.load(experiment_indices_file_path)
 
     net = build_resnet18(num_classes=100, norm_layer=torch.nn.BatchNorm2d)
     net.to(device)
-    cifar_data, cifar_data_loader = load_cifar_data(data_path)
+    cifar_data, cifar_data_loader = load_cifar_data(data_path, train=True)
+    test_data, test_data_loader = load_cifar_data(data_path, train=False)
 
     for exp_index in experiment_indices:
-
+        print("Experiment index: {0}".format(exp_index))
         ordered_classes = load_classes(class_order_dir_path, index=exp_index)
 
         average_weight_magnitude_per_epoch = np.zeros(number_of_epochs.size - 1, dtype=np.float32)
@@ -194,8 +268,9 @@ def analyze_results(results_dir: str, data_path: str, dormant_unit_threshold: fl
 
             prop_dormant, last_layer_features = compute_dormant_units_proportion(net, cifar_data_loader, dormant_unit_threshold)
             dormant_units_prop_after[i] = prop_dormant
-            effective_rank_after[i] = compute_effective_rank(last_layer_features)
-            stable_rank_after[i] = compute_stable_rank(last_layer_features)
+            singular_values = svd(last_layer_features, compute_uv=False, lapack_driver="gesvd")
+            effective_rank_after[i] = compute_effective_rank(singular_values)
+            stable_rank_after[i] = compute_stable_rank(singular_values)
 
             # compute summaries from data from previous tasks
             if i == 0: continue
@@ -204,17 +279,21 @@ def analyze_results(results_dir: str, data_path: str, dormant_unit_threshold: fl
             prop_dormant, last_layer_features = compute_dormant_units_proportion(net, cifar_data_loader, dormant_unit_threshold)
 
             dormant_units_prop_before[i] = prop_dormant
-            effective_rank_before[i] = compute_effective_rank(last_layer_features)
-            stable_rank_before[i] = compute_stable_rank(last_layer_features)
+            singular_values = svd(last_layer_features, compute_uv=False, lapack_driver="gesvd")
+            effective_rank_before[i] = compute_effective_rank(singular_values)
+            stable_rank_before[i] = compute_stable_rank(singular_values)
 
-        print("Experiment index: {0}".format(exp_index))
-        print("Average weight magnitude:\n\t{0}".format(average_weight_magnitude_per_epoch))
-        print("Proportion of dead units on previous tasks:\n\t{0}".format(dormant_units_prop_before))
-        print("Effective Rank on previous tasks:\nt\t{0}".format(effective_rank_before))
-        print("Stable Rank on previous tasks:\nt\t{0}".format(stable_rank_before))
-        print("Proportion of dead units on next task:\n\t{0}".format(dormant_units_prop_after))
-        print("Effective Rank on next task:\nt\t{0}".format(effective_rank_after))
-        print("Stable Rank on next task:\nt\t{0}".format(stable_rank_after))
+        net.load_state_dict(load_model_parameters(parameter_dir_path, exp_index, last_epoch))
+        accuracy_per_class_in_order = compute_last_task_accuracy_per_class_in_order(net, ordered_classes,
+                                                                                    test_data_loader, exp_index)
+
+        store_analysis_results(weight_magnitude_results=average_weight_magnitude_per_epoch,
+                               dormant_units_results=(dormant_units_prop_before, dormant_units_prop_after),
+                               effective_rank_results=(effective_rank_before, effective_rank_after),
+                               stable_rank_results=(stable_rank_before, stable_rank_after),
+                               accuracy_per_class_in_order=accuracy_per_class_in_order,
+                               results_dir=results_dir,
+                               experiment_index=exp_index)
 
 
 def parse_arguments() -> dict:
