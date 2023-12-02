62d9d31b241b64a8310f0b5f873eae02813fec17
Jfernando4 <jfhernan@ualberta.ca>
2023-12-02 16:49:29 -0700

    bugfix: no longer partitions current data

--- src/utils/data_management.py
+++ src/utils/data_management.py
@@ -14,6 +14,5 @@ def subsample_cifar_data_set(sub_sample_indices, cifar_data: CifarDataSet):
 
     cifar_data.data["data"] = cifar_data.data["data"][sub_sample_indices]
     cifar_data.data["labels"] = cifar_data.data["labels"][sub_sample_indices]
-    cifar_data.current_data["data"] = cifar_data.current_data["data"][sub_sample_indices]
-    cifar_data.current_data["labels"] = cifar_data.current_data["labels"][sub_sample_indices]
     cifar_data.integer_labels = torch.tensor(cifar_data.integer_labels)[sub_sample_indices].tolist()
+    cifar_data.current_data = cifar_data.partition_data()
