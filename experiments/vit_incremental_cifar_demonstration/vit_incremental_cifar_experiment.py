c9ee93fa45bc16c33c7dab3f5a3f9bca7761df0c
Jfernando4 <jfhernan@ualberta.ca>
2024-01-20 03:50:01 -0700

    update: reduced num layers and hidden dim

--- experiments/vit_incremental_cifar_demonstration/vit_incremental_cifar_experiment.py
+++ experiments/vit_incremental_cifar_demonstration/vit_incremental_cifar_experiment.py
@@ -92,9 +92,9 @@ class IncrementalCIFARExperiment(Experiment):
         self.net = VisionTransformer(
             image_size=32,
             patch_size=2,
-            num_layers=8,
+            num_layers=4,
             num_heads=12,
-            hidden_dim=768,
+            hidden_dim=384, #768,
             mlp_dim=768,#3072,
             num_classes=self.num_classes,
             dropout=0.1,
