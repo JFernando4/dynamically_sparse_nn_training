cab0c5bc91be0e6a360b32c69ce69611ae967d8d
Jfernando4 <jfhernan@ualberta.ca>
2024-01-21 14:47:47 -0700

    update: changed initialization to match torchvision initialization

--- src/networks/torchvision_vit_utils.py
+++ src/networks/torchvision_vit_utils.py
@@ -9,17 +9,17 @@ def initialize_vit(network: VisionTransformer):
     :return: None, but initializes the weights of the transformer model
     """
 
-    network.apply(xavier_normal_vit_initialization)
+    network.apply(xavier_vit_initialization)
     initialize_vit_heads(network.heads)
 
 
-def xavier_normal_vit_initialization(m: torch.nn.Module):
+def xavier_vit_initialization(m: torch.nn.Module):
     """
     Initializes the layers of a visual transformer except for the last layer
     """
-
     if isinstance(m, torch.nn.Conv2d):
-        torch.nn.init.xavier_normal_(m.weight)
+        fan_in = m.in_channels * m.kernel_size[0] * m.kernel_size[1]
+        torch.nn.init.trunc_normal_(m.weight, std=torch.math.sqrt(1 / fan_in))
         torch.nn.init.zeros_(m.bias)
     elif isinstance(m, EncoderBlock):
         initialize_layer_norm_module(m.ln_1)
@@ -43,11 +43,11 @@ def initialize_self_multihead_attention_module(m: torch.nn.MultiheadAttention):
     Initializes a multihead attention module using xavier normal initialization
     """
     if m._qkv_same_embed_dim:
-        torch.nn.init.xavier_normal_(m.in_proj_weight)
+        torch.nn.init.xavier_uniform_(m.in_proj_weight)
     else:
-        torch.nn.init.xavier_normal_(m.q_proj_weight)
-        torch.nn.init.xavier_normal_(m.k_proj_weight)
-        torch.nn.init.xavier_normal_(m.v_proj_weight)
+        torch.nn.init.xavier_uniform_(m.q_proj_weight)
+        torch.nn.init.xavier_uniform_(m.k_proj_weight)
+        torch.nn.init.xavier_uniform_(m.v_proj_weight)
 
     if m.in_proj_bias is not None:
         torch.nn.init.zeros_(m.in_proj_bias)
@@ -64,7 +64,7 @@ def initialize_mlp_block(m: MLPBlock):
     """
     for sub_m in m.modules():
         if isinstance(sub_m, torch.nn.Linear):
-            torch.nn.init.xavier_normal_(sub_m.weight)
+            torch.nn.init.xavier_uniform_(sub_m.weight)
             if sub_m.bias is not None:
                 torch.nn.init.normal_(sub_m.bias, std=1e-6)
 
@@ -74,9 +74,8 @@ def initialize_vit_heads(m: torch.nn.Sequential):
     Initializes the classification heads of a visual transformer
     """
 
-    torch.nn.init.xavier_normal_(m[0].weight)
+    torch.nn.init.zeros_(m[0].weight)
     torch.nn.init.zeros_(m[0].bias)
 
     if len(m) > 1:
-        torch.nn.init.xavier_normal_(m[2].weight)
-        torch.nn.init.zeros_(m[2].bias)
+        raise ValueError("Don't know how to handle heads with a representation layer.")
