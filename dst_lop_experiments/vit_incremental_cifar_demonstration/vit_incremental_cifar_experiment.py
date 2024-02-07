f4a1d11369e4e7420a3784157e406a447f41f81e
Jfernando4 <jfhernan@ualberta.ca>
2024-02-06 22:00:52 -0700

    changed hyperparameters in main

--- dst_lop_experiments/vit_incremental_cifar_demonstration/vit_incremental_cifar_experiment.py
+++ dst_lop_experiments/vit_incremental_cifar_demonstration/vit_incremental_cifar_experiment.py
@@ -443,8 +443,8 @@ def main():
         "momentum": 0.9,
         "dropout_prob": 0.1,
         "noise_std": 0.0,
-        "topology_update_freq": 2,
-        "sparsity": 0.1,
+        "topology_update_freq": 3,
+        "sparsity": 0.2
         "dst_method": "set_ds",
         "data_path": os.path.join(file_path, "data"),
         "num_epochs": 2000,
