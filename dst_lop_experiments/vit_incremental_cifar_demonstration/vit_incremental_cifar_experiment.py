324bbc71af32e9f5c787e5b8a2a7f4afc9c53917
Jfernando4 <jfhernan@ualberta.ca>
2024-02-10 07:16:45 -0700

    changed parameters in main

--- dst_lop_experiments/vit_incremental_cifar_demonstration/vit_incremental_cifar_experiment.py
+++ dst_lop_experiments/vit_incremental_cifar_demonstration/vit_incremental_cifar_experiment.py
@@ -469,7 +469,7 @@ def main():
         "topology_update_freq": 3,
         "sparsity": 0.1,
         "drop_fraction": 0.2,
-        "dst_method": "set_r",
+        "dst_method": "set_ds",
         "data_path": os.path.join(file_path, "data"),
         "num_epochs": 2000,
         "initial_num_classes": 5,
