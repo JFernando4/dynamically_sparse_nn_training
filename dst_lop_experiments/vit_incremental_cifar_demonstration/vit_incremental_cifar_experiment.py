e9e16362538f7894f4899f4770e4c0248b6a7c73
Jfernando4 <jfhernan@ualberta.ca>
2024-02-18 14:44:04 -0700

    include conv projection

--- dst_lop_experiments/vit_incremental_cifar_demonstration/vit_incremental_cifar_experiment.py
+++ dst_lop_experiments/vit_incremental_cifar_demonstration/vit_incremental_cifar_experiment.py
@@ -105,7 +105,7 @@ class IncrementalCIFARExperiment(Experiment):
         # initialize masks
         self.net_masks = None
         if self.sparse_network:
-            self.net_masks = init_vit_weight_masks(self.net, self.sparsity)
+            self.net_masks = init_vit_weight_masks(self.net, self.sparsity, include_conv_proj=True)
             apply_weight_masks(self.net_masks)
 
         # initialize optimizer and loss function
