87d1a04537a064dd51615b5cc4d9ae248efe7ad0
Jfernando4 <jfhernan@ualberta.ca>
2024-02-29 10:34:09 -0700

    changed how ln masks are handled

--- src/sparsity_functions/vit_sparsity_funcs.py
+++ src/sparsity_functions/vit_sparsity_funcs.py
@@ -32,11 +32,11 @@ def init_vit_weight_masks(net: VisionTransformer, sparsity_level: float, include
     """
 
     masks = []
+    ln_masks = []
     # generate mask for convolutional projection
     if include_conv_proj:
         conv_proj_mask = init_weight_mask_from_tensor(net.conv_proj.weight, sparsity_level)
         fan_in = net.conv_proj.in_channels * net.conv_proj.kernel_size[0] * net.conv_proj.kernel_size[1]
-        conv_proj_mask["is_layer_norm"] = False
         conv_proj_mask["init_func"] = lambda z: torch.nn.init.trunc_normal_(z, std=torch.math.sqrt(1 / fan_in))
         conv_proj_mask["init_std"] = torch.math.sqrt(1 / fan_in)
         masks.append(conv_proj_mask)
@@ -44,7 +44,6 @@ def init_vit_weight_masks(net: VisionTransformer, sparsity_level: float, include
     # generate mask for class_token parameters
     if include_class_token:
         class_token_mask = init_weight_mask_from_tensor(net.class_token, sparsity_level)
-        class_token_mask["is_layer_norm"] = False
         class_token_mask["init_func"] = torch.nn.init.xavier_normal_
         class_token_mask["init_std"] = get_xavier_init_std(net.class_token)
         masks.append(class_token_mask)
@@ -52,15 +51,16 @@ def init_vit_weight_masks(net: VisionTransformer, sparsity_level: float, include
     # generate mask for pos_embedding parameters
     if include_pos_embedding:
         pos_embedding_mask = init_weight_mask_from_tensor(net.encoder.pos_embedding, sparsity_level)
-        pos_embedding_mask["is_layer_norm"] = False
         pos_embedding_mask["init_func"] = lambda z: torch.nn.init.normal_(z, std=0.02)
         pos_embedding_mask["init_std"] = 0.02
         masks.append(pos_embedding_mask)
 
     # generate masks for encoder
-    masks.extend(init_vit_encoder_masks(net.encoder, sparsity_level, include_msa, include_ln))
+    encoder_weight_masks, encoder_ln_masks = init_vit_encoder_masks(net.encoder, sparsity_level, include_msa, include_ln)
+    masks.extend(encoder_weight_masks)
+    ln_masks.extend(encoder_ln_masks)
 
-    return masks
+    return masks, ln_masks
 
 
 def init_vit_encoder_masks(mod: Encoder, sparsity_level: float, include_msa: bool = False, include_ln: bool = False):
@@ -78,16 +78,19 @@ def init_vit_encoder_masks(mod: Encoder, sparsity_level: float, include_msa: boo
     """
 
     masks = []
+    ln_masks = []
 
     for e_block in mod.layers:
         assert isinstance(e_block, EncoderBlock)
-        masks.extend(init_encoder_block_masks(e_block, sparsity_level, include_msa, include_ln))
+        temp_weight_masks, temp_ln_masks = init_encoder_block_masks(e_block, sparsity_level, include_msa, include_ln)
+        masks.extend(temp_weight_masks)
+        ln_masks.extend(temp_ln_masks)
 
     if include_ln:
-        last_ln_mask = {"module": mod.ln, "is_layer_norm": True}
-        masks.append(last_ln_mask)
+        last_ln_mask = {"module": mod.ln}
+        ln_masks.append(last_ln_mask)
 
-    return masks
+    return masks, ln_masks
 
 
 def init_encoder_block_masks(mod: EncoderBlock, sparsity_level: float, include_msa: bool = False,
@@ -102,28 +105,28 @@ def init_encoder_block_masks(mod: EncoderBlock, sparsity_level: float, include_m
          include_ln: bool indicating whether to include layer norm in the masks list
 
     Returns:
-        list of dictionaries for each weight matrix of each layer in the encoder
+        list of dictionaries for each weight matrix of each layer in the encoder and list of dictionaries for each
+        layer norm layer. Layer norm list is empty if include layer norm is false.
     """
     masks = []
+    ln_masks = []
 
     # multi-head self-attention masks
     if include_msa:
         in_proj_mask = init_weight_mask_from_tensor(mod.self_attention.in_proj_weight, sparsity_level)
-        in_proj_mask["is_layer_norm"] = False
         in_proj_mask["init_func"] = torch.nn.init.xavier_normal_
         in_proj_mask["init_std"] = get_xavier_init_std(mod.self_attention.in_proj_weight)
         masks.append(in_proj_mask)
 
         out_proj_mask = init_weight_mask_from_tensor(mod.self_attention.out_proj.weight, sparsity_level)
-        out_proj_mask["is_layer_norm"] = False
         out_proj_mask["init_func"] = torch.nn.init.xavier_normal_
         out_proj_mask["init_std"] = get_xavier_init_std(mod.self_attention.out_proj.weight)
         masks.append(out_proj_mask)
 
     if include_ln:
-        l1_mask = {"module": mod.ln_1, "is_layer_norm": True}
-        l2_mask = {"module": mod.ln_2, "is_layer_norm": True}
-        masks.extend([l1_mask, l2_mask])
+        l1_mask = {"module": mod.ln_1}
+        l2_mask = {"module": mod.ln_2}
+        ln_masks.extend([l1_mask, l2_mask])
 
     # mlp block masks
     for sub_m in mod.mlp.modules():
@@ -134,5 +137,5 @@ def init_encoder_block_masks(mod: EncoderBlock, sparsity_level: float, include_m
             temp_mask["init_std"] = get_xavier_init_std(sub_m.weight)
             masks.append(temp_mask)
 
-    return masks
+    return masks, ln_masks
 
