9a6e231339cf322fb93cb4e3f335fd7fa75f7358
Jfernando4 <jfhernan@ualberta.ca>
2024-02-29 13:33:22 -0700

    handle not having affine transformation

--- src/networks/vit_init_functions.py
+++ src/networks/vit_init_functions.py
@@ -37,6 +37,7 @@ def initialize_layer_norm_module(m: torch.nn.Module):
     Initializes the weights of a layer norm module to one and the bias to zero
     """
     if not isinstance(m, torch.nn.LayerNorm): return
+    if not m.elementwise_affine: return
     torch.nn.init.ones_(m.weight)
     torch.nn.init.zeros_(m.bias)
 
