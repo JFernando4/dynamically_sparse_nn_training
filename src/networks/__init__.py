a3f221059ecbd5d1cddec09157ca38d7e1b876c7
Jfernado4 <jfhernan@ualberta.ca>
2024-04-02 14:15:14 -0600

    added resnet

--- src/networks/__init__.py
+++ src/networks/__init__.py
@@ -1,3 +1,5 @@
 from .vit_init_functions import initialize_vit, initialize_vit_heads, init_weight_regularization_list, \
     initialize_layer_norm_module
 from .regularized_sgd import RegularizedSGD
+from .res_gnt import ResGnT
+from .torchvision_modified_resnet import build_resnet18, kaiming_init_resnet_module, init_batch_norm_module
