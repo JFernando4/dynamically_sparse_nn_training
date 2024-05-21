1dba28aaa62d7493c26453d3e2504bb5dda95ea6
Jfernado4 <jfhernan@ualberta.ca>
2024-05-21 10:53:12 -0600

    removed hess_approx and redo from pruning functions change xavier grow function for kaiming

--- cbpw_experiments/permuted_mnist/permuted_mnist_experiment.py
+++ cbpw_experiments/permuted_mnist/permuted_mnist_experiment.py
@@ -65,8 +65,8 @@ class PermutedMNISTExperiment(Experiment):
         # CBPw parameters
         self.topology_update_freq = access_dict(exp_params, "topology_update_freq", default=0, val_type=int)
         self.epoch_freq = access_dict(exp_params, "epoch_freq", default=False, val_type=bool)
-        pruning_functions_names = ["none", "magnitude", "redo", "gf", "hess_approx"]
-        grow_methods = ["none", "pm_min", "xavier_normal", "zero"]
+        pruning_functions_names = ["none", "magnitude", "gf"]
+        grow_methods = ["none", "pm_min", "kaiming_normal", "zero"]
         self.prune_method = access_dict(exp_params, "prune_method", default="none", val_type=str,
                                         choices=pruning_functions_names)
         self.grow_method = access_dict(exp_params, "grow_method", default="none", val_type=str, choices=grow_methods)
