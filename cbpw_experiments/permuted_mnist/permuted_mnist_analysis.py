921afc5a1bbacf7dafbebaf91e15670855750b3b
Jfernado4 <jfhernan@ualberta.ca>
2024-08-03 10:36:45 -0600

    added a hack

--- cbpw_experiments/permuted_mnist/permuted_mnist_analysis.py
+++ cbpw_experiments/permuted_mnist/permuted_mnist_analysis.py
@@ -188,9 +188,10 @@ def insert_column_and_row_values(base_name: str, column_var: str, row_var: str, 
         if row_correct_next or column_correct_next:
             temp_part_split = temp_part.split("_")
             temp_part_split[0] = str(rv) if row_correct_next else str(cv)
-            if temp_part_split[1] == "normal":  # quick hack for when parameter values is "kaiming_normal"
-                temp_part_split.pop(1)
-            temp_part = "_".join(temp_part_split)
+            if len(temp_part_split) >= 2:
+                if temp_part_split[1] == "normal":  # quick hack for when parameter values is "kaiming_normal"
+                    temp_part_split.pop(1)
+                temp_part = "_".join(temp_part_split)
             column_correct_next = False
             row_correct_next = False
         if column_var in part:
