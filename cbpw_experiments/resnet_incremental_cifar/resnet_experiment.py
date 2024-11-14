db62144e9dc40f3da11d0d4c2a9bcf0ae7b984bc
Jfernado4 <jfhernan@ualberta.ca>
2024-11-14 11:29:20 -0700

    added option to use bottleneck layers

--- cbpw_experiments/resnet_incremental_cifar/resnet_experiment.py
+++ cbpw_experiments/resnet_incremental_cifar/resnet_experiment.py
@@ -12,7 +12,7 @@ import numpy as np
 from mlproj_manager.problems import CifarDataSet
 from mlproj_manager.util import access_dict
 
-from src import kaiming_init_resnet_module, build_resnet18, ResGnT, ResNet, init_batch_norm_module
+from src import kaiming_init_resnet_module, build_resnet18, ResGnT, ResNet, init_batch_norm_module, build_resnet18_bottleneck
 from src.cbpw_functions import initialize_weight_dict, update_weights, initialize_bn_list_resnet, setup_cbpw_layer_norm_update_function
 from src.plasticity_functions import inject_noise
 from src.utils import get_cifar_data, compute_accuracy_from_batch, parse_terminal_arguments
@@ -31,6 +31,7 @@ class ResNetIncrementalCIFARExperiment(IncrementalCIFARExperiment):
         self.momentum = exp_params["momentum"]
         self.use_lr_schedule = access_dict(exp_params, "use_lr_schedule", default=False, val_type=bool)
         self.rescaled_wd = access_dict(exp_params, "rescaled_wd", default=False, val_type=bool)
+        self.omit_bottleneck = access_dict(exp_params, "omit_bottleneck", default=False, val_type=bool)
 
         # network resetting parameters
         self.reset_head = access_dict(exp_params, "reset_head", default=False, val_type=bool)
@@ -70,7 +71,8 @@ class ResNetIncrementalCIFARExperiment(IncrementalCIFARExperiment):
 
         """ Network set up """
         # initialize network
-        self.net = build_resnet18(num_classes=self.num_classes, norm_layer=torch.nn.BatchNorm2d)
+        build_function = build_resnet18 if self.omit_bottleneck else build_resnet18_bottleneck
+        self.net = build_function(num_classes=self.num_classes, norm_layer=torch.nn.BatchNorm2d)
         self.net.apply(kaiming_init_resnet_module)
         self.net.to(self.device)
 
