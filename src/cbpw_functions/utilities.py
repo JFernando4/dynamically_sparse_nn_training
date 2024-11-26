bde37373981d7c9a53f246cd6966db82b35915ae
Jfernado4 <jfhernan@ualberta.ca>
2024-11-25 17:20:44 -0700

    deleted unused arguments

--- src/cbpw_functions/utilities.py
+++ src/cbpw_functions/utilities.py
@@ -72,20 +72,17 @@ def initialize_weights_dict_df_as_rate(net: Union[VisionTransformer, ResNet],
     Initializes the weight dictionaries used in CBPw for a network. The drop_factor is used as a rate, which
     is relevant if drop_factor * p.numel() is less than 1.
     """
-    weight_update_func = setup_cbpw_weight_update_function(prune_method, grow_method, drop_factor=drop_factor,
-                                                            as_rate=True, reinit_val=0.0)
-    output_update_func = setup_cbpw_weight_update_function(prune_method, "zero", drop_factor=drop_factor,
-                                                           as_rate=True)
-    bias_update_func = setup_cbpw_weight_update_function(prune_method, grow_name="zero", drop_factor=drop_factor,
-                                                         as_rate=True, reinit_val=0.0)
-    ln_weight_update_func = setup_cbpw_weight_update_function(prune_method, grow_name="fixed",
-                                                              drop_factor=drop_factor, as_rate=True, reinit_val=1.0)
+    weight_update_func = setup_cbpw_weight_update_function(prune_method, grow_method, drop_factor=drop_factor)
+    output_update_func = setup_cbpw_weight_update_function(prune_method, "zero", drop_factor=drop_factor)
+    bias_update_func = setup_cbpw_weight_update_function(prune_method, grow_name="zero", drop_factor=drop_factor)
+    ln_weight_update_func = setup_cbpw_weight_update_function(prune_method, grow_name="fixed", drop_factor=drop_factor,
+                                                              reinit_val=1.0)
 
     weight_dict = {}
     for n, p in net.named_parameters():
         is_weight = "weight" in n
         is_bias = "bias" in n
-        is_layer_or_batch_norm = (".ln_1." in n) or (".ln_2." in n) or ("bn3." in n) or ("bn1." in n) or ("bn2." in n) or ("downsample.1." in n)
+        is_layer_or_batch_norm = ("bn3." in n) or ("bn1." in n) or ("bn2." in n) or ("downsample.1." in n)
 
         if is_weight and is_layer_or_batch_norm:
             weight_dict[n] = (p, ln_weight_update_func)
