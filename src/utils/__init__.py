cea0bf2a972e76a767dcaef82c171701c1156a7d
Jfernado4 <jfhernan@ualberta.ca>
2024-12-13 13:53:31 -0700

    added new imports

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -7,7 +7,7 @@ from .evaluation_functions import compute_accuracy_from_batch, compute_average_g
     compute_matrix_rank_summaries, compute_average_weight_magnitude, bootstrapped_return
 # from .wandb_setup import initialize_wandb, get_wandb_id
 from .data_management import get_cifar_data, subsample_cifar_data_set
-from .experiment_utils import parse_terminal_arguments
+from .experiment_utils import parse_terminal_arguments, parse_plots_and_analysis_terminal_arguments
 from .cifar100_experiment_utils import *
 from .permuted_mnist_experiment_utils import compute_dead_units_proportion
-from .analysis_and_plotting import COLOR_DICT, aggregate_over_bins, plot_results
+from .analysis_and_plotting import COLOR_DICT, aggregate_over_bins, plot_results, plot_avg_with_shaded_region
