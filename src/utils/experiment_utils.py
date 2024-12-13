07716a5f1dca4e1262a62322408f7718d4a9d16e
Jfernado4 <jfhernan@ualberta.ca>
2024-12-13 13:49:11 -0700

    added function for parsing arguments

--- src/utils/experiment_utils.py
+++ src/utils/experiment_utils.py
@@ -11,3 +11,13 @@ def parse_terminal_arguments():
                                  help="This determines the random seed for the experiment.")
     argument_parser.add_argument("--verbose", action="store_true", default=False)
     return argument_parser.parse_args()
+
+
+def parse_plots_and_analysis_terminal_arguments():
+    """ Reads analysis arguments """
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument("--config_file", action="store", type=str, required=True,
+                                 help="JSON file with analysis configurations.")
+    argument_parser.add_argument("--save_plot", action="store_true", default=False)
+    argument_parser.add_argument("--debug", action="store_true", default=False)
+    return argument_parser.parse_args()
