91f0a81df0108ac1a2ac17a6fb32f8902d6d92e5
Jfernado4 <jfhernan@ualberta.ca>
2024-12-13 13:53:12 -0700

    added new plotting function

--- src/utils/analysis_and_plotting.py
+++ src/utils/analysis_and_plotting.py
@@ -93,3 +93,60 @@ def plot_results(results_data: dict, plot_parameters: dict, plot_dir: str, measu
     else:
         plt.show()
     plt.close()
+
+
+def plot_avg_with_shaded_region(results_data: dict = None, results_avg: dict = None, results_low: dict = None,
+                                results_high: dict = None, x_axis: dict = None, plot_parameters: dict = None,
+                                plot_dir: str = "", measurement_name: str = "", save_plots: bool = True,
+                                plot_name_prefix: str = "", num_samples: int = None):
+    """ Plots the data in results_data accoring to the parameters in plot_parameters """
+    assert (results_data is not None) or ((results_avg is not None) and (results_low is not None) and (results_high is not None))
+    os.makedirs(plot_dir, exist_ok=True)
+
+    color_order = access_dict(plot_parameters, "color_order", list(COLOR_DICT.keys()), list)
+    alpha = access_dict(plot_parameters, "alpha", 0.1, float)
+    x_label = access_dict(plot_parameters, "x_label", "Permutation Number", str)
+    y_label = access_dict(plot_parameters, "y_label", measurement_name, str)
+    labels = access_dict(plot_parameters, "labels", list(results_data.keys()), list)
+    linestyles = access_dict(plot_parameters, "linestyles", ["-"] * len(results_data), list)
+    ylim = access_dict(plot_parameters, "ylim", None)
+    yticks = access_dict(plot_parameters, "yticks", None)
+    xlim = access_dict(plot_parameters, "xlim", None)
+    visible_grid = access_dict(plot_parameters, "visible_grid", default=True, val_type=bool)
+
+    results_dict = results_data if results_data is not None else results_avg
+    for i, (pc, temp_results) in enumerate(results_dict.items()):
+        if results_data is not None:
+            temp_average = np.mean(temp_results, axis=0)
+            num_samples = temp_results.shape[0]
+            ste = np.std(temp_results, axis=0, ddof=1) / np.sqrt(num_samples)
+            temp_low, temp_high = temp_average - ste, temp_average + ste
+        else:
+            temp_average, temp_low, temp_high = results_avg[pc], results_low[pc], results_high[pc]
+
+        temp_x_axis = np.arange(temp_average.size) if x_axis is None else x_axis[pc]
+        print(f"\t{pc}\n\tNumber of samples: {num_samples}")
+        print(f"\tMax: {np.max(temp_average):.5f}\n\tMin: {np.min(temp_average):.5f}")
+        measurement_name += f"_sample_size_{num_samples}"
+
+        plt.plot(temp_x_axis, temp_average, label=labels[i], color=COLOR_DICT[color_order[i]], linestyle=linestyles[i])
+        plt.fill_between(temp_x_axis, temp_low, temp_high, color=COLOR_DICT[color_order[i]], alpha=alpha)
+
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
+    plt.legend()
+    plt.grid(visible=visible_grid, axis="y")
+
+    if ylim is not None:
+        plt.ylim(ylim)
+    if xlim is not None:
+        plt.xlim(xlim)
+    if yticks is not None:
+        plt.yticks(yticks)
+
+    if save_plots:
+        plot_path = os.path.join(plot_dir, f"{plot_name_prefix}_{measurement_name}.svg")
+        plt.savefig(plot_path, dpi=200)
+    else:
+        plt.show()
+    plt.close()
