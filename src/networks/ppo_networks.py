6716733bdc56349887dc1834402b36cee61af973
Jfernado4 <jfhernan@ualberta.ca>
2025-02-24 20:55:24 -0700

    added option to use shifted ln

--- src/networks/ppo_networks.py
+++ src/networks/ppo_networks.py
@@ -6,10 +6,9 @@ from torch import nn
 import torch
 from torch.distributions import Normal
 
-from mlproj_manager.util.neural_networks import init_weights_kaiming
-
 from .cbp_layer import CBPLinear
 from .redo_layer import ReDoLinear
+from .shifted_layer_norm import ShiftedLayerNorm
 
 
 def register_hook(net: nn.Module, hook_fn):
@@ -37,7 +36,8 @@ class TwoLayerNetwork(nn.Module):
                  reinit_frequency: int = 0,
                  reinit_threshold: float = 0.0,
                  decay_rate: float = 0.99,
-                 use_ln: bool = True):
+                 use_ln: bool = True,
+                 use_shifted_ln: bool = False):
         """
         Two-hidden-layer network with continual backpropagation and ReDo for PPO experiments
         """
@@ -46,18 +46,19 @@ class TwoLayerNetwork(nn.Module):
         self.act_type = act_type
         self.activation_func = {'Tanh': nn.Tanh, 'ReLU': nn.ReLU, 'elu': nn.ELU, 'sigmoid': nn.Sigmoid}[self.act_type]
         self.use_ln = use_ln
+        layer_norm_class = ShiftedLayerNorm if use_shifted_ln else nn.LayerNorm
 
         # first layer
         self.ff_1 = nn.Linear(input_dim, h_dim)
         self.act_1 = self.activation_func()
         self.reinit_layer_1 = None
-        self.ln_1 = nn.LayerNorm(h_dim) if self.use_ln else None
+        self.ln_1 = layer_norm_class(h_dim) if self.use_ln else None
 
         # second layer
         self.ff_2 = nn.Linear(h_dim, h_dim)
         self.act_2 = self.activation_func()
         self.reinit_layer_2 = None
-        self.ln_2 = nn.LayerNorm(h_dim) if self.use_ln else None
+        self.ln_2 = layer_norm_class(h_dim) if self.use_ln else None
 
         # output layer
         self.out = nn.Linear(h_dim, output_dim)
@@ -65,14 +66,14 @@ class TwoLayerNetwork(nn.Module):
         if use_cbp:
             assert maturity_threshold > 0 and replacement_rate > 0.0
             kw_arguments = {"maturity_threshold": maturity_threshold, "replacement_rate": replacement_rate,
-                            "util_type": "contribution", "decay_rate": decay_rate}
+                            "util_type": "contribution", "decay_rate": decay_rate, "use_shifted_ln": use_shifted_ln}
             self.reinit_layer_1 = CBPLinear(in_layer=self.ff_1, out_layer=self.ff_2, ln_layer=self.ln_1, **kw_arguments)
             self.reinit_layer_1 = CBPLinear(in_layer=self.ff_2, out_layer=self.out, ln_layer=self.ln_2, **kw_arguments)
 
         if use_redo:
             assert reinit_frequency > 0 and reinit_threshold > 0.0
             kw_arguments = {"reinit_frequency": reinit_frequency, "reinit_threshold": reinit_threshold,
-                            "util_type": "original", "decay_rate": decay_rate}
+                            "util_type": "original", "decay_rate": decay_rate, "use_shifted_ln": use_shifted_ln}
             self.reinit_layer_1 = ReDoLinear(in_layer=self.ff_1, out_layer=self.ff_2, ln_layer=self.ln_1, **kw_arguments)
             self.reinit_layer_2 = ReDoLinear(in_layer=self.ff_2, out_layer=self.out, ln_layer=self.ln_2, **kw_arguments)
 
@@ -104,7 +105,7 @@ class TwoLayerNetwork(nn.Module):
         return x
 
 
-def initialize_two_layer_network(net: TwoLayerNetwork):
+def initialize_two_layer_network(net: TwoLayerNetwork, use_shifted_ln: bool = False):
     """ Initializes network using kaiming uniform distribution """
     act = "relu" if net.act_type == "elu" else net.act_type.lower()
 
@@ -117,7 +118,10 @@ def initialize_two_layer_network(net: TwoLayerNetwork):
 
     for layer in ln_layers:
         if layer is not None:
-            torch.nn.init.ones_(layer.weight)
+            if use_shifted_ln:
+                torch.nn.init.zeros_(layer.weight)
+            else:
+                torch.nn.init.ones_(layer.weight)
             torch.nn.init.zeros_(layer.bias)
 
     torch.nn.init.zeros_(net.out.weight)
@@ -136,7 +140,8 @@ class MLPVF(nn.Module):
                  reinit_frequency: int = 0,
                  reinit_threshold: float = 0.0,
                  decay_rate: float = 0.99,
-                 use_ln: bool = True):
+                 use_ln: bool = True,
+                 use_shifted_ln: bool = False):
 
         super().__init__()
 
@@ -144,8 +149,7 @@ class MLPVF(nn.Module):
         self.v_net = TwoLayerNetwork(input_dim=input_dim, output_dim=1, h_dim=h_dim, act_type=act_type,
                                      use_cbp=use_cbp, maturity_threshold=maturity_threshold, replacement_rate=replacement_rate,
                                      use_redo=use_redo, reinit_frequency=reinit_frequency, reinit_threshold=reinit_threshold,
-                                     decay_rate=decay_rate, use_ln=use_ln)
-        initialize_two_layer_network(self.v_net)
+                                     decay_rate=decay_rate, use_ln=use_ln, use_shifted_ln=use_shifted_ln)
         self.v_net.to(device)
 
     def value(self, x, activations: list = None):
@@ -168,7 +172,8 @@ class MLPPolicy(nn.Module):
                  reinit_frequency: int = 0,
                  reinit_threshold: float = 0.0,
                  decay_rate: float = 0.99,
-                 use_ln: bool = True):
+                 use_ln: bool = True,
+                 use_shifted_ln: bool = False):
         super().__init__()
 
         self.act_type = act_type
@@ -177,7 +182,7 @@ class MLPPolicy(nn.Module):
         self.mean_net = TwoLayerNetwork(input_dim=input_dim, output_dim=a_dim, h_dim=h_dim, act_type=act_type,
                                         use_cbp=use_cbp, maturity_threshold=maturity_threshold, replacement_rate=replacement_rate,
                                         use_redo=use_redo, reinit_frequency=reinit_frequency, reinit_threshold=reinit_threshold,
-                                        decay_rate=decay_rate, use_ln=use_ln)
+                                        decay_rate=decay_rate, use_ln=use_ln, use_shifted_ln=use_shifted_ln)
 
         self.log_std = nn.Parameter(torch.ones(a_dim) * log_std)
         self.mean_net.to(device)
