38117f002b91cd5945f4d68a5fda0399c0036f84
Jfernado4 <jfhernan@ualberta.ca>
2025-02-24 20:24:04 -0700

    bugfix: wrong condition

--- cbpw_experiments/experiment_analysis/incremental_cifar_analysis.py
+++ cbpw_experiments/experiment_analysis/incremental_cifar_analysis.py
@@ -114,7 +114,7 @@ def compute_average_weight_magnitude(state_dict: dict):
     ln_sum, ln_numel, sa_sum, sa_numel, mlp_sum, mlp_numel, total_sum, total_numel = 0, 0, 0, 0, 0, 0, 0, 0
     sa_weights, mlp_weights = [], []
     for n, p in state_dict.items():
-        if not p.requires_grad: continue
+        if ".cbp." in n: continue
         is_weight = "weight" in n
         is_self_attention = ".self_attention." in n
         is_layer_norm = (".ln." in n) or (".ln_1." in n) or (".ln_2." in n)
