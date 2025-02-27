2f79ccb1329b0b7d25e592a6b88e8dd9c8633fb3
Jfernado4 <jfhernan@ualberta.ca>
2025-02-27 11:33:59 -0700

    added functionality to keep track of how many weights are reset

--- src/networks/ppo_networks.py
+++ src/networks/ppo_networks.py
@@ -42,7 +42,8 @@ class TwoLayerNetwork(nn.Module):
         Two-hidden-layer network with continual backpropagation and ReDo for PPO experiments
         """
         super().__init__()
-
+        self.use_cbp = use_cbp
+        self.use_redo = use_redo
         self.act_type = act_type
         self.activation_func = {'Tanh': nn.Tanh, 'ReLU': nn.ReLU, 'elu': nn.ELU, 'sigmoid': nn.Sigmoid}[self.act_type]
         self.use_ln = use_ln
@@ -53,24 +54,26 @@ class TwoLayerNetwork(nn.Module):
         self.act_1 = self.activation_func()
         self.reinit_layer_1 = None
         self.ln_1 = layer_norm_class(h_dim) if self.use_ln else None
+        self.weights_per_feature_1 = input_dim + h_dim
 
         # second layer
         self.ff_2 = nn.Linear(h_dim, h_dim)
         self.act_2 = self.activation_func()
         self.reinit_layer_2 = None
         self.ln_2 = layer_norm_class(h_dim) if self.use_ln else None
+        self.weights_per_feature_2 = h_dim + output_dim
 
         # output layer
         self.out = nn.Linear(h_dim, output_dim)
 
-        if use_cbp:
+        if self.use_cbp:
             assert maturity_threshold > 0 and replacement_rate > 0.0
             kw_arguments = {"maturity_threshold": maturity_threshold, "replacement_rate": replacement_rate,
                             "util_type": "contribution", "decay_rate": decay_rate, "use_shifted_ln": use_shifted_ln}
             self.reinit_layer_1 = CBPLinear(in_layer=self.ff_1, out_layer=self.ff_2, ln_layer=self.ln_1, **kw_arguments)
-            self.reinit_layer_1 = CBPLinear(in_layer=self.ff_2, out_layer=self.out, ln_layer=self.ln_2, **kw_arguments)
+            self.reinit_layer_1 = CBPLinear(in_layer=self.ff_2, out_layer=self.out, ln_layer=self.ln_2, **kw_arguments)     #TODO: this is a big bug
 
-        if use_redo:
+        if self.use_redo:
             assert reinit_frequency > 0 and reinit_threshold > 0.0
             kw_arguments = {"reinit_frequency": reinit_frequency, "reinit_threshold": reinit_threshold,
                             "util_type": "original", "decay_rate": decay_rate, "use_shifted_ln": use_shifted_ln}
@@ -103,6 +106,24 @@ class TwoLayerNetwork(nn.Module):
         x = self.out(x)
 
         return x
+
+    def feature_replace_event_indicator(self):
+        if not self.use_cbp and not self.use_redo: return False
+
+        return (self.reinit_layer_1.replace_feature_event_indicator or
+                self.reinit_layer_2.replace_feature_event_indicator)
+
+    def reset_indicators(self):
+        if not self.use_cbp and not self.use_redo: return
+
+        self.reinit_layer_1.reset_indicators()
+        self.reinit_layer_2.reset_indicators()
+
+    def num_replaced(self):
+        if not self.use_redo and not self.use_cbp: return (0, 0, 0)
+        nr_1 = 0.0 if not self.reinit_layer_1.replace_feature_event_indicator else self.reinit_layer_1.num_replaced
+        nr_2 = 0.0 if not self.reinit_layer_2.replace_feature_event_indicator else self.reinit_layer_2.num_replaced
+        return nr_1 * self.weights_per_feature_1, nr_2 * self.weights_per_feature_2
 
 
 def initialize_two_layer_network(net: TwoLayerNetwork, use_shifted_ln: bool = False):
