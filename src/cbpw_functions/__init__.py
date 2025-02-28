a98fcf6eb59d48bde513d34bab5abd88e2dee968
Jfernado4 <jfhernan@ualberta.ca>
2025-02-28 09:17:25 -0700

    added import

--- src/cbpw_functions/__init__.py
+++ src/cbpw_functions/__init__.py
@@ -1,4 +1,4 @@
-from .weight_matrix_updates import (setup_cbpw_weight_update_function, update_weights,
+from .weight_matrix_updates import (setup_cbpw_weight_update_function, update_weights, compute_utility,
                                     setup_cbpw_layer_norm_update_function, compute_trace_utility)
 from .utilities import initialize_weight_dict, initialize_bn_list_resnet, initialize_ln_list_bert
 from .swr_optimizer import SelectiveWeightReinitializationSGD, get_init_parameters
