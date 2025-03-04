744ff7e0133b4ffc2c27b92542fcb9130712a161
Jfernado4 <jfhernan@ualberta.ca>
2025-03-03 19:07:52 -0700

    added debug print

--- cbpw_experiments/experiment_analysis/incremental_cifar_analysis.py
+++ cbpw_experiments/experiment_analysis/incremental_cifar_analysis.py
@@ -189,6 +189,7 @@ def analyse_results(analysis_parameters: dict, save_plots: bool = True):
             results_data = get_results_data_accuracy_diff(results_dir, parameter_combinations, base_lines, excluded_indices, max_samples)
             plot_results(results_data, plot_parameters, plot_dir, sn, save_plots, plot_name_prefix)
         elif sn in WEIGHT_SUMMARY_NAMES:
+            print(excluded_indices)
             if compute_weight_magnitude_summaries:
                 for param_comb in parameter_combinations:
                     compute_and_store_weight_magnitude_results(param_comb, results_dir)
